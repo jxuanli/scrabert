4170b607c3093e5fa5936cabddd0916d30fdeb2e
junxua01 <junxua01@students.cs.ubc.ca>
2022-06-27 18:16:49 -0700

    Only browse the truste domains!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use select::document::Document;
 use select::predicate::Name;
 use std::collections::HashSet;
 use std::error::Error;
-use std::io::Cursor;
+use std::io::{Cursor, prelude::*, BufReader};
+use std::fs::File;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -16,7 +17,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("{}", url);
     }
     urls = set.into_iter().collect();
-    let contents = get_contents(&urls[0]).await?;
+    let contents = get_contents(&urls[0][..]).await?;
     for content in contents {
         println!("{}", content);
     }
@@ -37,7 +38,7 @@ async fn google_search_url(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
         .map(str::to_string)
         .filter_map(|x| {
             let tmp: Vec<&str> = (*x).split("/url?q=").collect();
-            if tmp[1].len() == 0 || !tmp[1].contains("en.wikipedia.org"){
+            if tmp[1].len() == 0 || !is_trusted("urls.txt",tmp[1]){
                 return None;
             }
             return Some(tmp[1].to_string())
@@ -46,13 +47,25 @@ async fn google_search_url(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(links)
 }
 
+fn is_trusted(path: &str, str: &str) -> bool {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let tmp = line.unwrap();
+        if str.contains(&tmp[..]) {
+            return true;
+        }
+    }
+    false
+}
+
 #[allow(dead_code)]
 async fn get_urls(starting_url: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut slices: Vec<&str> = starting_url.split("https://").collect();
     slices = slices[1].split("/").collect();
     let domain = slices[0];
     if domain.len() == 0 {
-        return Ok(Vec::new());
+        return Ok(Vec::new()); 
     }
     let html = get_html(starting_url).await?;
     let links: Vec<String> = html
