9fe3d7cb3563fd6fcef2bea2bf8ac71f90e32051
junxua01 <junxua01@students.cs.ubc.ca>
2022-06-27 17:27:07 -0700

    scape the google!

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::io::Cursor;
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut set: HashSet<String> = HashSet::new();
     let urls =
-        get_urls("https://stackoverflow.com/questions/16494822/why-is-it-called-rust").await?;
+        google_search_url("https://www.google.com/search?q=why+is+it+called+rust").await?;
     for url in urls {
         set.insert(url);
     }
@@ -23,6 +23,25 @@ async fn get_html(url: &str) -> Result<Document, Box<dyn Error>> {
     Ok(Document::from_read(Cursor::new(html))?)
 }
 
+async fn google_search_url(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let html = get_html(url).await?;
+    let links: Vec<String> = html
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .filter(|x| (*x).starts_with("/url?q="))
+        .map(str::to_string)
+        .filter_map(|x| {
+            let tmp: Vec<&str> = (*x).split("/url?q=").collect();
+            if tmp[1].len() == 0 || !tmp[1].contains("en.wikipedia.org"){
+                return None;
+            }
+            return Some(tmp[1].to_string())
+        })
+        .collect();
+    Ok(links)
+}
+
+#[allow(dead_code)]
 async fn get_urls(starting_url: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut slices: Vec<&str> = starting_url.split("https://").collect();
     slices = slices[1].split("/").collect();
