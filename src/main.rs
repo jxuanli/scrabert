a69908a367598911105a69a11a119b1a2d2ca4d2
junxua01 <junxua01@students.cs.ubc.ca>
2022-07-05 19:13:10 -0700

    added spinner

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ mod summarizer;
 
 use anyhow::Result;
 use futures::executor;
+use spinners_rs::{Spinner, Spinners};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let tmp = executor::block_on(scraper::scrape()).unwrap();
+    let mut sp = Spinner::new(Spinners::Dots9, "\t\t I am thinking!");
+    sp.start();
     let (_handle, classifier) = summarizer::Summarizer::spawn();
     let summarization = classifier.predict(tmp).await?;
-    println!("Results: {:?}", summarization);
+    sp.stop();
+    println!("\n Results: {:?}", summarization);
     Ok(())
 }
