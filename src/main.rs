d500b8f8837c48bffe96c1c8225d1e1eb45ab217
junxua01 <junxua01@students.cs.ubc.ca>
2022-07-06 17:03:49 -0700

    refactored

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,7 @@ use std::collections::HashSet;
 #[tokio::main]
 async fn main() -> Result<()> {
     let contents = executor::block_on(scraper::scrape()).unwrap();
-    // let mut sp = Spinner::new(Spinners::Dots6, "\t\t I am thinking!");
-    // sp.start();
-    // let mut tmp: HashSet<Vec<String>> = HashSet::new();
-    // for content in contents {
-    //     let (_handle, classifier) = summarizer::Summarizer::spawn();
-    //     let summarization = classifier.predict(content).await?;
-    //     tmp.insert(summarization);
-    // }
-    // sp.stop();
-    // let summarizations: Vec<String> = tmp.iter().filter_map(|x| Some((*x)[0].replace("[X_SEP]", "").replace("  ", " "))).collect();
-    // for s in summarizations {
-    //     println!("{:?}", s);
-    // }
-    get_summaries(contents);
-    Ok(())
+    get_summaries(contents).await
 }
 
 async fn get_summaries(contents: Vec<Vec<String>>) -> Result<()> {
